790f80e107a1bf33a1e51c550cee8ec1a2a8147b
arsanav <arsanav@gmail.com>
2023-08-25 17:22:28 +0300

    update pickle operations

--- main/classification.py
+++ main/classification.py
@@ -6,17 +6,16 @@ from nltk.stem import WordNetLemmatizer
 
 import pickle
 from main.preproc import text_preprocessor, text_lemmatizer
+vectorizer_filename = os.getcwd() + "/models/vectorizer.pkl"
+model_filename = os.getcwd() + "/models/model.pkl"
+
+with open(vectorizer_filename, 'rb') as file:
+    pickle_vectorizer = pickle.load(file)
+
+with open(model_filename, 'rb') as file:
+    pickle_model = pickle.load(file)
 
 
 def make_prediction(text):
-    vectorizer_filename = os.getcwd() + "/models/vectorizer.pkl"
-    model_filename = os.getcwd() + "/models/model.pkl"
-
-    with open(vectorizer_filename, 'rb') as file:
-        pickle_vectorizer = pickle.load(file)
-
-    with open(model_filename, 'rb') as file:
-        pickle_model = pickle.load(file)
-
     x_pred = pickle_vectorizer.transform(text)
     return pickle_model.predict(x_pred)
--- manage.py
+++ manage.py
@@ -8,6 +8,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sentiment_analysis_test.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
